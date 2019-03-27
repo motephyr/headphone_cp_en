621c08340dd367f3e94014f933ef7902c2618ce8
motephyr <motephyr@gmail.com>
2019-03-27 21:08:02 +0800

    Purchase suggestion

--- app/Controllers/Http/HomeController.js
+++ app/Controllers/Http/HomeController.js
@@ -3,6 +3,8 @@ const RawContent = use('App/Models/RawContent')
 const AnalyzeService = require("../../Services/AnalyzeService")
 const Stock = use('App/Models/Stock')
 const Record = use('App/Models/Record')
+const TargetContent = use('App/Models/TargetContent')
+
 /**
  * Resourceful controller for interacting with homes
  */
@@ -23,9 +25,23 @@ class HomeController {
     raw_contents = AnalyzeService.get_result(raw_contents)
     raw_contents = raw_contents.filter(function(x){return x.state !== 'buy'})
 
+    raw_contents = raw_contents.map(async function(x){
+      let target = await TargetContent.findBy('name', x.name)
+      x.buy_it = ""
+      if(target){
+        x.buy_it = 'target'
+        if(x.price < target.mean_price - target.stdev_price){
+          x.buy_it = 'mustBuy'
+        }
+      }
+      return x
+    })
+
     let stocks = await Stock.query().orderBy('id', 'desc').fetch()
 
     let records = await Record.query().orderBy('id', 'desc').fetch()
+
+    raw_contents = await Promise.all(raw_contents)
 
     return view.render('home.index',{raw_contents: raw_contents, stocks: stocks.toJSON(), records: records.toJSON()})
 
--- start/routes.js
+++ start/routes.js
@@ -16,7 +16,7 @@
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use('Route')
 
-Route.on('/').render('welcome')
+Route.get('/', 'HomeController.index')
 Route.get('/home', 'HomeController.index')
 Route.post('/crawler/get_data', 'CrawlerController.get_data')
 Route.get('/crawler/analyze_data', 'CrawlerController.analyze_data')
